1309247e8dc7f7ecbfb836eea9c01946ff02daef
Carlton Stith <carlton215@gmail.com>
2019-01-23 10:44:56 -0500

    Updates

---
+++ js/jquery.flickrush.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+		
+			$(function() {
+				$('div#flickrImages').flickrush({
+					limit:6,
+					id:'66357606@N05',
+					random:true
+				}); 
+			});			
+
+</script>
